a4047581d0835550e0c64a49ac86d2c998504da1
Diogenes do Prado Lima <diogenesdopradolima@gmail.com>
2023-10-04 21:43:08 -0300

    Add project files.

---
+++ GerenciadorDeEmpregados/Empregado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeEmpregados
+{
+    internal class Empregado
+    {
+        private int Matricula { get; set; }
+        private string Nome { get; set; }
+        private string Sobrenome { get; set; }
+        private int Idade { get; set; }
+        private DateOnly DataNascimento { get; set; }
+        private DateTime DataContratacao { get; set; }
+
+        private double Salario { get; set; }
+
+
+    }
+}
---
+++ GerenciadorDeEmpregados/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GerenciadorDeEmpregados
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+        }
+
+    }
+}
